8fc64126a8413d87730c84d7cdf85b6cbca54734
Tom Andrews <git@tomandrews.co.uk>
2013-12-18 15:40:26 +0000

    Add command line switches

--- server.go
+++ server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -11,23 +12,29 @@ import (
 )
 
 var statsdClient *gostats.StatsdClient
-
+var verbose bool
+var port int
 
 func main() {
-	port := 3000
+	flag.IntVar(&port, "port", 3000, "Port for the server to listen")
+	flag.BoolVar(&verbose, "verbose", false, "Set true to see all stats received")
+	flag.Parse()
+
 	convertedPort := ":" + strconv.Itoa(port)
 	log.Printf("Starting server on port %s", convertedPort)
 	http.HandleFunc("/", handleRequest)
 	statsdClient = gostats.New("localhost", 8125)
+
 	log.Fatal(http.ListenAndServe(convertedPort, nil))
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	stat := stat.FromRequest(r)
-	log.Println(stat.String())
-	//log.Println("Sending stat to statsd")
+	if verbose {
+		log.Println(stat.String())
+	}
 	statsdClient.Timing(stat.Name, int64(stat.Value))
-	//log.Println("Timing sent to statsd")
+
 	w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
 	fmt.Fprintf(w, ";")
 }
