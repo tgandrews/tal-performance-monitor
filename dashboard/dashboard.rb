89f0e7524b597783f8c5695f90481bd48356403a
Tom Andrews <git@tomandrews.co.uk>
2013-12-19 14:40:26 +0000

    Show all stat names as list and link to page showing the filtered statistics

--- dashboard/dashboard.rb
+++ dashboard/dashboard.rb
@@ -3,16 +3,28 @@ require 'mongo'
 module Dashboard
 	class App < Sinatra::Application
 		get '/' do
-			@stats = connect_to_mongo_stats
-			all_stats = @stats.find.to_a
-			haml :index, :locals => {:all_stats => all_stats} 
+			haml :index, :locals => {:all_stats => get_statistics_as_array} 
 		end
 
-		def connect_to_mongo_stats
+		get '/display/:stat_name' do
+			name = params[:stat_name]
+			filter = { "name" => name }
+			haml :index, :locals => {:all_stats => get_statistics_filtered_as_array(filter)}
+		end
+
+		def get_statistics
 			puts 'Connecting to mongo db...'
 			mongo_client = Mongo::MongoClient.new("localhost")
-			@mongo_db = mongo_client.db('tpm')
-			return @mongo_db.collection('application_stats')
+			mongo_db = mongo_client.db('tpm')
+			return mongo_db.collection('application_stats')
+		end
+
+		def get_statistics_as_array
+			get_statistics.find.to_a
+		end
+
+		def get_statistics_filtered_as_array(filter)
+			get_statistics.find(filter).to_a
 		end
 	end
 end
