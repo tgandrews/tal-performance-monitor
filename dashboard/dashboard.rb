15f2ce8f1640f60dd0f9984db58b9f09721a835c
Tom Andrews <git@tomandrews.co.uk>
2013-12-19 14:22:22 +0000

    Show the stats that are being recored in mongo

--- dashboard/dashboard.rb
+++ dashboard/dashboard.rb
@@ -1,10 +1,18 @@
-require 'sinatra'
-require 'haml'
+require 'mongo'
 
 module Dashboard
 	class App < Sinatra::Application
 		get '/' do
-			haml :index
+			@stats = connect_to_mongo_stats
+			all_stats = @stats.find.to_a
+			haml :index, :locals => {:all_stats => all_stats} 
+		end
+
+		def connect_to_mongo_stats
+			puts 'Connecting to mongo db...'
+			mongo_client = Mongo::MongoClient.new("localhost")
+			@mongo_db = mongo_client.db('tpm')
+			return @mongo_db.collection('application_stats')
 		end
 	end
 end
