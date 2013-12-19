10bbacf63e1c5fe8044f4740881ce00960edd74b
Tom Andrews <git@tomandrews.co.uk>
2013-12-19 15:07:40 +0000

    Show the statistics for the selected name

--- dashboard/dashboard.rb
+++ dashboard/dashboard.rb
@@ -7,15 +7,15 @@ module Dashboard
 		end
 
 		get '/display/:stat_name' do
-			name = params[:stat_name]
-			filter = { "name" => name }
-			haml :index, :locals => {:all_stats => get_statistics_filtered_as_array(filter)}
+			stat_name = params[:stat_name]
+			filter = { "name" => stat_name }
+			haml :display, :locals => {:all_stats => get_statistics_filtered_as_array(filter), :stat_name => stat_name}
 		end
 
 		def get_statistics
-			puts 'Connecting to mongo db...'
 			mongo_client = Mongo::MongoClient.new("localhost")
 			mongo_db = mongo_client.db('tpm')
+
 			return mongo_db.collection('application_stats')
 		end
 
