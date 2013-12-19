bb33b0655dc2fcf236835f64c38548629d3f1b6c
Tom Andrews <git@tomandrews.co.uk>
2013-12-19 15:56:05 +0000

    We now have graphs!

--- dashboard/dashboard.rb
+++ dashboard/dashboard.rb
@@ -9,7 +9,14 @@ module Dashboard
 		get '/display/:stat_name' do
 			stat_name = params[:stat_name]
 			filter = { "name" => stat_name }
-			haml :display, :locals => {:all_stats => get_statistics_filtered_as_array(filter), :stat_name => stat_name}
+			
+			stats = get_statistics_filtered_as_array(filter)
+
+			stats_with_date = stats.select { |s| !s["date"].nil? }
+			dates = stats_with_date.map { |s| "'" + format_date(s["date"]) + "'"}.join(",")
+			values = stats_with_date.map { |s| s["value"].to_s }.join(",")
+
+			haml :display, :locals => {:stat_name => stat_name, :dates => dates, :values => values}
 		end
 
 		def get_statistics
@@ -25,6 +32,10 @@ module Dashboard
 
 		def get_statistics_filtered_as_array(filter)
 			get_statistics.find(filter).to_a
+		end
+
+		def format_date(time)
+			time.strftime("%H:%M:%S %d %b %Y") 
 		end
 	end
 end
